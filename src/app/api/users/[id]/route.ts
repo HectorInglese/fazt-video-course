9a425aee480d21be688f00c0d73ae72ecd90b131
Hector Manuel Inglese Vivas <hectoringlese@gmail.com>
2023-09-28 16:27:37 -0400

    cambiado redireccion de back

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,11 +1,10 @@
-import { notFound } from 'next/navigation';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
   const { searchParams } = new URL(req.url);
   const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`);
   if (res.status === 404) {
-    notFound();
+    return NextResponse.redirect(new URL('/not-found', req.url));
   } else if (res.status === 200) {
     const data = await res.json();
     return NextResponse.json(data);
